d50b206eb912414b1cd01fbce087038d6320a97d
有你心安BUG <2608086516@qq.com>
2020-08-19 20:24:59 +0800

    empty

---
+++ components/empty/empty.js
@@ -0,0 +1,30 @@
+// components/empty/empty.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    image:{
+      type:String,
+      value:"./images/empty-image.png"
+    },
+    description:{
+      type:String,
+      value:"暂无更多"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
