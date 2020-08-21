6eded4623dd806573a8a79949a36847eaa33b6d5
有你心安BUG <2608086516@qq.com>
2020-08-21 14:40:18 +0800

    notice bar

---
+++ components/notice-bar/notice-bar.js
@@ -0,0 +1,91 @@
+// components/notice-bar/notice-bar.js
+/**
+   * notice数组
+   * color: 颜色
+   * background 滚动条背景
+   * mode 为fade是淡入淡出
+   */
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    notice:{
+      type:Array,
+      value:[]
+    },
+    leftIcon:{
+      type:String,
+      value:'./images/notice-icon.png'
+    },
+    showIcon:{
+      type:Boolean,
+      value:false
+    },
+    background:{
+      type:String,
+      value:"#fffbe8"
+    },
+    color:{
+      type:String,
+      value:"#ed6a0c"
+    },
+    size:{
+      type:String,
+      value:"28rpx"
+    },
+    textAlign:{
+      type:String,
+      value:"left"
+    },
+    mode:{
+      type:String,
+      value:""
+    }
+  },
+  lifetimes: {
+    attached() {
+      if(this.data.mode=='fade'){
+        console.log('ee');
+        this.setNoticeAnimate();
+      }
+     
+    },
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    noticeIndex:0,
+    isHideNotice:false
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    preventDefault(){},
+    setNoticeAnimate() {
+      this.removeNoticeAnimate();
+      this.data.noticeInterval = setInterval(() => {
+        this.setData({
+          isHideNotice: true,
+        });
+      }, 3000);
+    },
+    removeNoticeAnimate() {
+      clearInterval(this.data.noticeInterval);
+    },
+    noticeAnimateEnd() {
+      console.log('进来');
+      if (this.data.isHideNotice) {
+        let { notice, noticeIndex } = this.data;
+        console.log('isHideNotice',notice,noticeIndex);
+        this.setData({
+          noticeIndex: noticeIndex < notice.length - 1 ? noticeIndex + 1 : 0,
+          isHideNotice: false,
+        });
+      }
+    },
+  }
+})
